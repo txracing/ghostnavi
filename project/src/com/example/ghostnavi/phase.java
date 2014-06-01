a20c822ca0069a976662eae46aebd00f145d4a29
inuhin <inuchin@gmail.com>
2014-06-01 14:40:52 +0900

    add Volley Sources.
    change Layout(Resouce Message use)
    
    dummy button(View toggle check)

---
+++ project/src/com/example/ghostnavi/phase.java
@@ -0,0 +1,12 @@
+package com.example.ghostnavi;
+
+public class phase {
+
+	public static final int navistart = 0;
+	public static final int wheretogo = 1;
+	public static final int choosespeed = 2;
+	public static final int inmanytimes = 3;
+	public static final int okletsgo = 4;
+
+
+}
